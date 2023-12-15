40cd9d1c80bdbe938dc95b4bc4b399d24dfc059d
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2023-12-15 20:07:01 +0300

    stable

--- src/routes/profile/[id].page.tsx
+++ src/routes/profile/[id].page.tsx
@@ -3,27 +3,25 @@ import { ProfileUserInfo } from "./components/ProfileUserInfo";
 import { useUser } from "@/lib/rakkas/hooks/useUser";
 import { useQueries, useQuery } from "@tanstack/react-query";
 import { tryCatchWrapper } from "@/utils/helpers/async";
-import { ChevronLeft, ExternalLink, Mail } from "lucide-react";
+import { ChevronLeft } from "lucide-react";
 import { ProfileTabs } from "./components/ProfileTabs";
 import {
   getFollowerscount,
   getFollowingCount,
 } from "@/state/models/friends/friends";
-import { Icons } from "@/components/icons/Iconts";
 import { ProfileInfoLoader } from "./components/loaders/ProfileInfoloader";
 
-
-export default function OneProfilePage({params}: PageProps) {
+export default function OneProfilePage({ params }: PageProps) {
   const { user: logged_in, pb } = useUser();
   const profile_id = params.id;
   const profile_query = useQuery({
     queryKey: ["profile", profile_id],
     queryFn: () =>
-      tryCatchWrapper(pb.collection("pocketbook_user").getOne(profile_id,{})),
+      tryCatchWrapper(pb.collection("pocketbook_user").getOne(profile_id, {})),
   });
-  
-  const follower_count_key = ["profile","followers", profile_id];
-  const following_count_key = ["profile","following", profile_id];
+
+  const follower_count_key = ["profile", "followers", profile_id];
+  const following_count_key = ["profile", "following", profile_id];
 
   const count_query = useQueries({
     queries: [
@@ -38,20 +36,21 @@ export default function OneProfilePage({params}: PageProps) {
     ],
   });
 
-  
   const followers_count = count_query[0].data?.data ?? 0;
   const following_count = count_query[1].data?.data ?? 0;
-  
+
   // console.log({followers_count, following_count});
 
   const profile_user = profile_query.data?.data;
   return (
-    <div className="flex flex-col items-center  h-[99vh]  gap-3 overflow-y-scroll p-2 ml-5 
-    overflow-x-clip">
-      <Head  
-      title={profile_user?.username??"profile"}
-      description={profile_user?.bio??"Pocketbook profile"}
-      og:image={profile_user?.avatar??""}
+    <div
+      className="flex flex-col items-center  h-[99vh]  gap-3 overflow-y-scroll p-2 ml-5 
+    overflow-x-clip"
+    >
+      <Head
+        title={profile_user?.username ?? "profile"}
+        description={profile_user?.bio ?? "Pocketbook profile"}
+        og:image={profile_user?.avatar ?? ""}
       />
       <div className="w-[95%]  flex gap-2 items-center sticky top-0 z-50 ">
         {/* <Link href="-1"> */}
@@ -61,30 +60,27 @@ export default function OneProfilePage({params}: PageProps) {
           size={10}
         />
         {/* </Link> */}
-        <h1 className="text-3xl font-bold">Profile</h1>
-      H</div>
-   
-   {(profile_query.isPending) ? 
-   <ProfileInfoLoader/>
-   :(<div className="w-full ">
-        {(profile_user && profile_user) && (
-          <ProfileUserInfo
-            profile_user={profile_user}
-            logged_in_user={logged_in}
-          />
-        )}
+        <h1 className="text-3xl font-bold">Profile</h1>H
       </div>
+
+      {profile_query.isPending ? (
+        <ProfileInfoLoader />
+      ) : (
+        <div className="w-full ">
+          {profile_user && profile_user && (
+            <ProfileUserInfo
+              profile_user={profile_user}
+              logged_in_user={logged_in}
+            />
+          )}
+        </div>
       )}
 
-
-        <ProfileTabs
-          profile_id={profile_id}
-          followers_count={followers_count}
-          following_count={following_count}
-        />
-      
+      <ProfileTabs
+        profile_id={profile_id}
+        followers_count={followers_count}
+        following_count={following_count}
+      />
     </div>
   );
 }
-
-
